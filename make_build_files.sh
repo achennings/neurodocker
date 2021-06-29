b1fb7f5d1399cbc0da659213d507fb994124454f
achennings <achennings@utexas.edu>
2021-06-29 16:00:38 -0500

    install libgsl-dev

--- make_build_files.sh
+++ make_build_files.sh
@@ -17,14 +17,37 @@ generate_docker() {
     --env FS_LICENSE=/home/docs/license.txt \
     --run-bash "fs_install_mcr R2014b" \
     --install libncurses5 \
+    --install libgsl-dev \
     --miniconda \
           use_env=base \
           conda_install='python=3.8 matplotlib numpy pandas scikit-learn nilearn scipy seaborn traits' \
           pip_install='nipype pingouin brainiak ipython' \
     --user neuro
 }
-# generate_singularity () {
-#  sudo docker run repronim/neurodocker:master generate singularity \
-# }
+generate_singularity () {
+  sudo docker run repronim/neurodocker:master generate singularity \
+    --base=neurodebian:buster --pkg-manager=apt \
+    --install apt_opts="--quiet" vim libopenmpi-dev libcurl4-openssl-dev libxml2-dev libssl-dev libudunits2-dev libv8-dev \
+    --afni version=latest method=binaries \
+    --run-bash "apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF' && echo deb http://cloud.r-project.org/bin/linux/debian buster-cran40/ >> /etc/apt/sources.list && apt update && apt install -y -t buster-cran40 r-base r-base-dev" \
+    --run-bash "rPkgsInstall -pkgs ALL -site 'https://cran.microsoft.com/'" \
+    --run-bash "apt -y upgrade" \
+    --fsl version=6.0.4 method=binaries \
+    --run-bash "bash /opt/fsl-6.0.4/etc/fslconf/fslpython_install.sh -f /opt/fsl-6.0.4" \
+    --ants version=2.3.1 method=binaries \
+    --dcm2niix version=2bf2e482aec8e9959c6bd8e833cdccba3607c617 method=source \
+    --convert3d version=1.0.0 method=binaries \
+    --freesurfer version=7.1.1 method=binaries \
+    --copy license.txt /home/docs/license.txt \
+    --env FS_LICENSE=/home/docs/license.txt \
+    --run-bash "fs_install_mcr R2014b" \
+    --install libncurses5 \
+    --install libgsl-dev \
+    --miniconda \
+          use_env=base \
+          conda_install='python=3.8 matplotlib numpy pandas scikit-learn nilearn scipy seaborn traits' \
+          pip_install='nipype pingouin brainiak ipython' \
+    --user neuro
+}
 generate_docker > Dockerfile
-# generate_singularity > Singularity
+generate_singularity > Singularity
